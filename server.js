eb59729bee3de9e729d19fb0dcf1697d875713f7
SAVAGEBEAN <88079234+SAVAGEBEAN@users.noreply.github.com>
2022-07-22 20:51:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser')
 const path = require('node:path')
-require('dotenv/config');
+
 
 const app = express();
 app.use(express.json())
